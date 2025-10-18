88d5265b853defbe303249fa3880d323b66d4dd3
morronejoaquin <morronejoaquinmdq@gmail.com>
2025-10-18 15:06:41 -0300

    Commit Header,Footer y enrutamiento

--- Workspace/src/app/app.routes.ts
+++ Workspace/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomePage } from './pages/AboutUs/home-page/home-page';
+import { AccountPage } from './pages/Users/account-page/account-page';
+import { MakeOrderPage } from './pages/Orders/make-order-page/make-order-page';
+import { PriceCalculatorPage } from './pages/AboutUs/price-calculator-page/price-calculator-page';
+import { WherePage } from './pages/AboutUs/where-page/where-page';
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePage },
+  { path: 'account', component:AccountPage},
+  { path: 'make-order', component: MakeOrderPage },
+  { path: 'price-calculator', component: PriceCalculatorPage },
+  { path: 'contact', component: WherePage },
+  { path: '**', redirectTo: 'home' }
+];
